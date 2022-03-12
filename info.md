09bafd28ff730a822b91d5210086ffd04f2820e3
Sergey-Kukharenko <Kuharenko@inventive.ru>
2022-03-12 09:45:07 +0300

    init

---
+++ info.md
@@ -0,0 +1,13 @@
+## lesson
+
+``` bash
+10
+```
+
+## time
+
+``` bash
+00.00.00
+```
+
+[sdras github](https://github.com/sdras/building-web-apps-with-vue)
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="container">
+    <h1>Hi from home Page</h1>
+    <AppHeader />
+    <AppRestaurantInfo />
+  </main>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader";
+import AppRestaurantInfo from "@/components/AppRestaurantInfo";
+
+export default {
+  components: {
+    AppHeader,
+    AppRestaurantInfo
+}
+};
+</script>
+
+<style scoped>
+
+</style>
